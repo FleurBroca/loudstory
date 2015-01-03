663c5f8c18295d31c4c36c19a109008edd9b2fd0
Fleur Broca <brocafleur@gmail.com>
2015-01-03 15:43:36 +0100

    modified storyboard and fixed CSS bugs

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,4 +3,12 @@ class DashboardsController < ApplicationController
     @user = current_user
     @members = current_team.users
   end
+
+  def team_user_dashboard
+    @user = User.find(params[:user_id])
+    @team = Team.find(params[:team_id])
+    @members = @team.users
+
+    render :index
+  end
 end
