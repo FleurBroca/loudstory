aa345b33a18a58032fc2dfbc1bbda92922c5fe33
Fleur Broca <brocafleur@gmail.com>
2015-01-23 00:11:05 +0100

    new email Mandrill

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "brocafleur@gmail.com"
+  default from: "fleur@loudstory.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
