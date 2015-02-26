b59c512651e4929b795829297a14e55e76637725
b3nty <benty1988@hotmail.com>
2015-02-26 22:49:39 +0100

    swap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -91,6 +91,24 @@ $(document).ready(function(){
       }
     });
   });
+
+
+  var txt = ['startup story', 'landing page', 'press kit', 'pitch contest'],
+    n = txt.length,
+    $swap = $('#swap'),
+    $span,
+    c = -1;
+
+  // CREATE SPANS INSIDE SPAN
+  for(var i=0; i<txt.length; i++) $swap.append($('<span />',{text:txt[i]}));
+  // HIDE AND COLLECT THEM
+  $span = $("span", $swap).hide(); 
+
+  (function loop(){
+    c = ++c % n;
+    $swap.animate({width: $span.eq( c ).width() });
+    $span.stop().fadeOut().eq(c).fadeIn().delay(1000).show(0, loop);
+  }()); 
 });
 
 
